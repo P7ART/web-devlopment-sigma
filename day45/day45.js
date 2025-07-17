9bdf7288d0e21393cc89f3913c01e86ba6cd1282
P7ART <kushagrasingh001001@gmail.com>
2025-07-17 17:37:00 +0530

    JAVASCRIPT INTRODUCTION

---
+++ day45/day45.js
@@ -0,0 +1,14 @@
+alert("hello world");
+   
+var a = prompt("Enter your name");
+var isTrue=confirm("are you sure you want to blast your computer")
+
+if(isTrue){
+    console.log("you are a fool");
+}else{
+    console.log("you are a genius");
+}
+console.log("hey your number is "  + a);    
+document.title = "hello world"
+
+document.body.style.backgroundColor = "red";

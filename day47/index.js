61a749a6c05e33bc4eb008fc95ab0e5d3c9355aa
P7ART <kushagrasingh001001@gmail.com>
2025-07-19 01:51:51 +0530

    CONDITIONALS IN  JAVASCRIPTS AND TERNORY OPERATOR

---
+++ day47/index.js
@@ -0,0 +1,61 @@
+console.log("today javascript  conditionals")
+
+
+// let a = 17;
+// let benifit = 2;
+// // Assignment Operators
+// a+= benifit; // a = a + benifit; //increment operator
+// a/= benifit; // a = a / benifit; //decrement operator
+// console.log("a is: " + a);
+// // Arithmetic Operators
+// console.log("Arithmetic Operators: ");
+// console.log(a+benifit);
+// console.log(a - benifit);
+// console.log(a * benifit);
+// console.log(a / benifit);
+// console.log(a % benifit); // use for only give remainder of a number division
+// console.log(a ** benifit); // power  operator
+
+
+// if ((a+benifit) != 18) {
+//     console.log(" you can drive ");
+// }
+
+// else{
+//     console.log(" you cannot drive ");
+// }
+
+// // comment in javascript which will be ignored by the compiler
+// // this is a single line comment
+// /*
+// this is a multi line comment
+// you can write anything here                 
+// */
+
+// //if else if  else ladder
+
+let age = 0;
+
+if (age ==18) {
+    console.log("you can driver")
+}
+else if (age == 0){
+    console.log("you are a kidding ??")
+
+}
+
+else if (age==1){
+    console.log("you are kidding again??")
+}
+
+else{
+    console.log("you cannot drive")
+}
+
+// ternory operator
+let a = 4;
+let b =2;
+
+let  c =(a>b)? 'a is greater' : 'b is greater'
+console.log(c);
+// ternory mei ? if hota h : else hota h issme condition phale dete h

9eeb4f8422c748f2664bd827f1d0b94c036749bc
P7ART <kushagrasingh001001@gmail.com>
2025-07-25 15:42:02 +0530

    javascript DOM BOM INTRODUCTION

---
+++ day55/day55.js
@@ -0,0 +1,9 @@
+console.log("DOCUMENT OBJECT MODEL");
+
+document.title = "Day 55: DOM Manipulation";
+console.log(document.title);
+console.log(document.body);
+document.body.style.backgroundColor = "lightblue";
+window.alert("Welcome to Day 55 of 100 Days of JavaScript!");
+window.document.body.innerHTML = "<h1>Welcome to the DOM Manipulation Day!</h1>";
+
